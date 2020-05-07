c5f9579af8a28349975863b698c4de37ac802307
Bethany Polovitz <bethany.polovitz@gmail.com>
2020-05-07 12:41:30 -0500

    rendering data to the files

--- Master/client/src/index.js
+++ Master/client/src/index.js
@@ -1,7 +1,9 @@
+//Require dependencies 
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-
+//rendering the App to the root element on the html page
 ReactDOM.render(<App />, document.getElementById("root"));
+//register serviceworker
 registerServiceWorker();
